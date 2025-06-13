97553b8e21c1baf4b3c82aa36f014bd72e5fbd70
Estefania Capitao <estefania.capitao@gmail.com>
2025-06-13 13:27:59 +0200

    modification page d'accueil, création de pages et mise à jour des routes.

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,13 @@
 import Route from "./Route.js";
 
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/Pages/home.html")];
+export const allRoutes = [
+  new Route("/", "Accueil", "/Pages/home.html"),
+  new Route("/login", "Connexion/Deconnexion", "/Pages/login.html"),
+  new Route("/register", "Inscription", "/Pages/register.html"),
+  new Route("/profile", "Profil", "/Pages/profile.html"),
+  new Route("/legalNotice", "Mentions légales", "/Pages/legalNotice.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
